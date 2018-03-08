3a9a7e66adcf72dffcd3fb1c8b72762084f7bde2
RafalPC <r.jenczelewski@gmail.com>
2018-03-08 10:43:25 +0100

    Finally made versions work with each other. Propably will cause problems in future, we are using older versions. Also updated gitignore and added basic screens.

---
+++ native-frontend/screens/ProfileScreen/ProfileScreen.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import {Text} from 'react-native'
+
+class ProfileScreen extends Component {
+    render() {
+        return <Text>Profile</Text>
+    }
+}
+
+export default ProfileScreen
