e1c66e49de759ee589822b34ca3e10de597e67f5
RafalPC <r.jenczelewski@gmail.com>
2018-03-11 12:25:21 +0100

    MAP IS DISPLAYING. Trying to make gitignore work

--- native-frontend/screens/MapScreen/MapScreen.js
+++ native-frontend/screens/MapScreen/MapScreen.js
@@ -1,18 +1,30 @@
 import React, {Component} from 'react'
 import {Text, View, StyleSheet} from 'react-native'
+import MapView from 'react-native-maps'
 
 class MapScreen extends Component {
     render() {
-        return <View style={styles.container}><Text>Mapa</Text></View>
+        return <View style={styles.container}><MapView style={styles.map}
+            initialRegion={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            }}
+        /></View>
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
+        ...StyleSheet.absoluteFillObject,
+        height: 400,
+        width: 400,
+        justifyContent: 'flex-end',
         alignItems: 'center',
-        justifyContent: 'center',
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
     },
 });
 
