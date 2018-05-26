98d58774d20c503efcb2cfd360cb7325a7d74c5e
Rafal Jenczelewski <r.jenczelewski@gmail.com>
2018-05-26 20:54:12 +0200

    Requsts should now contain user info

--- frontend/shared/store/store.js
+++ frontend/shared/store/store.js
@@ -3,9 +3,9 @@ import {applyMiddleware, createStore, combineReducers} from 'redux'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 
-import {routes, markers, userToken} from '../reducers/reducers'
+import {routes, markers, userInfo} from '../reducers/reducers'
 
-const reducers = combineReducers({routes, markers, userToken});
+const reducers = combineReducers({routes, markers, userInfo});
 const store = createStore(reducers, applyMiddleware(thunk, logger));
 
 export default store;
