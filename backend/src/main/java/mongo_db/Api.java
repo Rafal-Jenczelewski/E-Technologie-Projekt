624658179a93e4d2696134f20216689078d05ad6
Rafal Jenczelewski <rafal.jenczelewski@nokia.com>
2018-05-29 10:24:03 +0200

    Access token is now taken from headers, response has proper code and message.

--- backend/src/main/java/mongo_db/Api.java
+++ backend/src/main/java/mongo_db/Api.java
@@ -15,35 +15,41 @@ import java.io.InputStream;
 import java.io.StringWriter;
 
 
-public class Api {
+public class Api
+{
 
     private static MongoService mongo = new MongoService();
     private static Gson gsonTransformer = new Gson();
     private static String baseVerifyURL = "graph.facebook.com/debug_token?input_token=";
     private static String appID = "1661504553926840";
 
-    private static String buildVerifyURL(String userToken)
+
+    private static String buildVerifyURL( String userToken )
     {
         String url = baseVerifyURL;
         url += userToken + "&access_token=" + appID;
         return url;
     }
 
+
     private static CloseableHttpClient httpClient = HttpClients.createDefault();
 
-    public static boolean verify(String token){
-        HttpGet getRequest = new HttpGet( buildVerifyURL( token ));
+
+    public static boolean verify( String token )
+    {
+        HttpGet getRequest = new HttpGet( buildVerifyURL( token ) );
         try
         {
             CloseableHttpResponse response = httpClient.execute( getRequest );
             HttpEntity entity = response.getEntity();
             InputStream stream = entity.getContent();
 
-            StringWriter writer = new StringWriter(  );
+            StringWriter writer = new StringWriter();
             IOUtils.copy( stream, writer );
             String responseBody = writer.toString();
 
-            FacebookVerifyResponseBody body = gsonTransformer.fromJson( responseBody, FacebookVerifyResponseBody.class );
+            FacebookVerifyResponseBody body = gsonTransformer.fromJson( responseBody,
+                            FacebookVerifyResponseBody.class );
             return body.isIs_valid();
         }
         catch( IOException e )
@@ -53,108 +59,125 @@ public class Api {
         return false;
     }
 
-    public static void main(String[] args) {
 
-        post("/addSomeData", (req, res) -> {
+    public static void main( String[] args )
+    {
+
+        post( "/addSomeData", ( req, res ) -> {
             mongo.clear();
 
-            Marker marker = new Marker(51.1222822, 17.060590, "Nice place!", "opis_0", true, 1L);
-            mongo.addMarker(marker);
+            Marker marker = new Marker( 51.1222822, 17.060590, "Nice place!", "opis_0", true, 1L );
+            mongo.addMarker( marker );
 
-            marker = new Marker(51.1845689, 51.1845689, "Big tree!", "opis_1", false, 1L);
-            mongo.addMarker(marker);
+            marker = new Marker( 51.1845689, 51.1845689, "Big tree!", "opis_1", false, 1L );
+            mongo.addMarker( marker );
 
-            marker = new Marker(51.18415689, 17.0545594, "I like this!", "opis_2", true, 1L);
-            mongo.addMarker(marker);
+            marker = new Marker( 51.18415689, 17.0545594, "I like this!", "opis_2", true, 1L );
+            mongo.addMarker( marker );
 
-            marker = new Marker(51.18145689, 17.0514594, "Best shop on Citadel!", "opis_3", true, 1L);
-            mongo.addMarker(marker);
+            marker = new Marker( 51.18145689, 17.0514594, "Best shop on Citadel!", "opis_3", true,
+                            1L );
+            mongo.addMarker( marker );
 
-            marker = new Marker(51.18172689, 17.0514524, "Come here", "opis_4", false, 1L);
-            mongo.addMarker(marker);
+            marker = new Marker( 51.18172689, 17.0514524, "Come here", "opis_4", false, 1L );
+            mongo.addMarker( marker );
 
-            Coordinates[] temp = {new Coordinates(51.18112689, 17.0511524), new Coordinates(51.18472689, 17.0714524)};
-            Route route = new Route(temp, "That's amazing!", "opis_0", false,1L);
-            mongo.addRoute(route);
+            Coordinates[] temp = { new Coordinates( 51.18112689, 17.0511524 ),
+                                   new Coordinates( 51.18472689, 17.0714524 ) };
+            Route route = new Route( temp, "That's amazing!", "opis_0", false, 1L );
+            mongo.addRoute( route );
 
-            Coordinates[] temp1 = {new Coordinates(51.14112689, 17.0141524), new Coordinates(51.18485689, 17.0772524)};
-            route = new Route(temp1, "Nice walk!", "opis_1", false, 1L);
-            mongo.addRoute(route);
+            Coordinates[] temp1 = { new Coordinates( 51.14112689, 17.0141524 ),
+                                    new Coordinates( 51.18485689, 17.0772524 ) };
+            route = new Route( temp1, "Nice walk!", "opis_1", false, 1L );
+            mongo.addRoute( route );
 
             return "example data added";
-        }, gsonTransformer::toJson);
+        }, gsonTransformer::toJson );
 
-        post("/addMarker", (req, res) -> {
-            if(verify(req.queryParams("ACCESS-TOKEN"))) {
-                res.type("application/json");
-                Marker marker = gsonTransformer.fromJson(req.body(), Marker.class);
-                return mongo.addMarker(marker);
-            } else {
-                return "error-wrong-token";
+        post( "/addMarker", ( req, res ) -> {
+            if( verify( req.headers( "ACCESS-TOKEN" ) ) )
+            {
+                res.type( "application/json" );
+                Marker marker = gsonTransformer.fromJson( req.body(), Marker.class );
+                return mongo.addMarker( marker );
             }
-        }, gsonTransformer::toJson);
-
-        post("/addRoute", (req, res) -> {
-            if(verify(req.queryParams("ACCESS-TOKEN"))) {
-            res.type("application/json");
-            Route route = gsonTransformer.fromJson(req.body(), Route.class);
-            return mongo.addRoute(route);
-            } else {
-                return "error-wrong-token";
+            else
+            {
+                res.status( 401 );
+                return "User is not logged in.";
             }
-        }, gsonTransformer::toJson);
+        }, gsonTransformer::toJson );
 
-        post("/addComment", (req, res) -> {
-            if(verify(req.queryParams("ACCESS-TOKEN"))) {
-                res.type("application/json");
-                Comment comment = gsonTransformer.fromJson(req.body(), Comment.class);
-                return mongo.addComment(comment);
-            } else {
-                return "error-wrong-token";
+        post( "/addRoute", ( req, res ) -> {
+            if( verify( req.headers( "ACCESS-TOKEN" ) ) )
+            {
+                res.type( "application/json" );
+                Route route = gsonTransformer.fromJson( req.body(), Route.class );
+                return mongo.addRoute( route );
             }
-        }, gsonTransformer::toJson);
+            else
+            {
+                res.status( 401 );
+                return "User is not logged in.";
+            }
+        }, gsonTransformer::toJson );
 
-        get("/getAllMarkers", (req, res) -> {
-            res.type("application/json");
+        post( "/addComment", ( req, res ) -> {
+            if( verify( req.headers( "ACCESS-TOKEN" ) ) )
+            {
+                res.type( "application/json" );
+                Comment comment = gsonTransformer.fromJson( req.body(), Comment.class );
+                return mongo.addComment( comment );
+            }
+            else
+            {
+                res.status( 401 );
+                return "User is not logged in.";
+            }
+        }, gsonTransformer::toJson );
+
+        get( "/getAllMarkers", ( req, res ) -> {
+            res.type( "application/json" );
             return mongo.getAllMarkers();
-        }, gsonTransformer::toJson);
+        }, gsonTransformer::toJson );
 
-
-        get("/getAllRoutes", (req, res) -> {
-            res.type("application/json");
+        get( "/getAllRoutes", ( req, res ) -> {
+            res.type( "application/json" );
             return mongo.getAllRoutes();
-        }, gsonTransformer::toJson);
+        }, gsonTransformer::toJson );
 
-        get("/getMarkers", (req, res) -> {
-            System.out.println(req.queryParams("getOthers"));
-            System.out.println(req.queryParams("userID"));
-            boolean owned = Boolean.valueOf(req.queryParams("getOthers"));
-            Long userID = Long.valueOf(req.queryParams("userID"));
-            res.type("application/json");
+        get( "/getMarkers", ( req, res ) -> {
+            System.out.println( req.queryParams( "getOthers" ) );
+            System.out.println( req.queryParams( "userID" ) );
+            boolean owned = Boolean.valueOf( req.queryParams( "getOthers" ) );
+            Long userID = Long.valueOf( req.queryParams( "userID" ) );
+            res.type( "application/json" );
 
-            return mongo.getMarkers(owned, userID);
-        }, gsonTransformer::toJson);
+            return mongo.getMarkers( owned, userID );
+        }, gsonTransformer::toJson );
 
-        get("/getComments", (req, res) -> {
-            System.out.println(req.queryParams("markerId"));
-            String markerId =  gsonTransformer.fromJson(req.body(), String.class);
-            res.type("application/json");
-            return mongo.getComments(markerId);
-        }, gsonTransformer::toJson);
+        get( "/getComments", ( req, res ) -> {
+            System.out.println( req.queryParams( "markerId" ) );
+            String markerId = gsonTransformer.fromJson( req.body(), String.class );
+            res.type( "application/json" );
+            return mongo.getComments( markerId );
+        }, gsonTransformer::toJson );
 
-        get("/getRoutes", (req, res) -> {
-            boolean owned = Boolean.valueOf(req.queryParams("getOthers"));
-            Long userID = Long.valueOf(req.queryParams("userID"));
-            res.type("application/json");
+        get( "/getRoutes", ( req, res ) -> {
+            boolean owned = Boolean.valueOf( req.queryParams( "getOthers" ) );
+            Long userID = Long.valueOf( req.queryParams( "userID" ) );
+            res.type( "application/json" );
 
-            return mongo.getRoutes(owned, userID);
-        }, gsonTransformer::toJson);
+            return mongo.getRoutes( owned, userID );
+        }, gsonTransformer::toJson );
 
-        put("/changeStatus", (req, res) -> {
-            System.out.println(req.body());
-            ChangeStatusReqBody body = gsonTransformer.fromJson(req.body(), ChangeStatusReqBody.class);
-            mongo.changeStatus(body.getId(), body.isPublic());
+        put( "/changeStatus", ( req, res ) -> {
+            System.out.println( req.body() );
+            ChangeStatusReqBody body =
+                            gsonTransformer.fromJson( req.body(), ChangeStatusReqBody.class );
+            mongo.changeStatus( body.getId(), body.isPublic() );
             return "";
-        }, gsonTransformer::toJson);
+        }, gsonTransformer::toJson );
     }
 }
